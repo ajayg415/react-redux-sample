be9e270fa1d051d90766505443b8cee1dc93c9f4
ajayg415 <ajay.g415@gmail.com>
2020-01-28 22:29:50 +0530

    task completed with redux

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import './Display.css'
+
 const Display = props => {
   const { text } = props;
   console.log('text', text)
   return (
-    <h4>{text}</h4>
+    <h4 className='mt-4'>{text}</h4>
   );
 };
 
 const mapStateToProps = state => ({
-  text: state.text
+  text: state.search.text
 })
 
 export default connect(mapStateToProps)(Display);
